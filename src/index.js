260def2d64c1437d4b2e47a3c30ccdf6733cb52e
LINPINYAN <24019715@myrp.edu.sg>
2025-11-03 13:01:56 +0800

    edited pizza

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 
 //Component is just a JavaScript Function
 //Component name must start with a capital letter
@@ -11,26 +12,51 @@ function App() { //App is the parent, MyComponent is the child
     //declarative
 
     return(
-    <>
-        <Header/>
-        <Pizza/>
-    </>);
+    <div className="container">
+        <Header />
+        <Menu/>
+        <Footer />
+    </div>);
 }
 function Header() {
-    return <h1>Lin Pin Yan's Pizza Co.</h1>
-}
-function Pizza(){ 
-    //follow naming convention
     return (
-    <>
-    <img src="pizzas/spinaci.jpg"></img>
-    <h2>Spinach Pizza</h2>
-    <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    <div></div>
-    <p>10</p>
-    </>
+    <h1 style= {{ color: "orange", fontSize: "48px", textTransform: "uppercase"}}>Lin Pin Yan's Pizza Co.</h1>
     );
 }
+function Pizza(props){ 
+    //follow naming convention
+    //props is a JS object which is {image, name, ingredent, price}
+    return (
+    <div>
+    <img src={props.image} alt={props.aka}></img>
+    <h2>{props.name}</h2>
+    <p>{props.ingredent}</p>
+    <p>{props.price}</p>
+    </div>
+    );
+}
+
+function Menu(){
+    return(
+        <div className="menu">
+        <h2>Our Menu</h2>
+        <Pizza image="pizzas/spinaci.jpg" aka="Spinaci" name="Spinach Pizza" ingredent="Tomato, mozarella, spinach, and ricotta cheese" price={10}/>
+        <Pizza image="pizzas/spinaci.jpg" aka="Spinaci" name="Spinach Pizza" ingredent="Tomato, mozarella, spinach, and ricotta cheese" price={10}/>
+        </div>
+    );
+}
+
+function Footer(){
+    const hour = new Date().getHours();
+    return(
+        
+        <footer className="footer">
+            {hour >= 10 && hour < 22 ? "We're currently open.":"Sorry we're closed"}
+        </footer>
+    );
+}
+
+
 
 
 const root = ReactDom.createRoot(document.getElementById("root"));
